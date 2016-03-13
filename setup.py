e7921b465d61d33bba0922bb951d98a31382ee80
Joseph Kahn <JosephBKahn@gmail.com>
2016-03-13 15:08:11 -0400

    unittest2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_long_description():
 
 install_requires = ['flake8']
 
-test_requires = ['nose', 'flake8>=1.5']
+test_requires = ['nose', 'flake8>=1.5', 'unittest2==1.1.0']
 
 setup(
     name='flake8-debugger',
